9db36f3080bd95fc5a1542b18fd4831e410d8cc6
zhengweiwei <zhengweiwei@bytedance.com>
2024-07-12 17:42:36 +0800

    add AddFragment's menu, UpdateFragment's menu

--- app/src/main/java/com/example/mytodo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/mytodo/fragments/add/AddFragment.kt
@@ -3,6 +3,8 @@ package com.example.mytodo.fragments.add
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mytodo.R
@@ -13,8 +15,17 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // 打开选项菜单
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    // 打开选项菜单时将其与add_fragment_menu绑定
+    // inflate方法用于将XML布局文件转换成对应的视图对象View object
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
